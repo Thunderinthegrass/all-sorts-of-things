0525fa84dbbec7d14a4f48dd0d064c23b6030272
ThunderInTheGrass <propeller8924@gmail.com>
2021-01-22 00:18:40 +0800

    add sliders

---
+++ app/js/sliders/infinity-slider1.js
@@ -0,0 +1,17 @@
+let images = document.querySelectorAll('.infinity-slider .img-wrapper');
+let current = 0;
+
+function slider() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.add('opacity0');
+  }
+  images[current].classList.remove('opacity0');
+  if (current+1 == images.length) {
+    current = 0;
+  }
+  else{
+    current++;
+  }
+}
+
+document.querySelector('.infinity-slider').onclick = slider;
