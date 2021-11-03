69868adbbd54e74abe6995906d41cf7e6463522b
ThunderInTheGrass <propeller8924@gmail.com>
2021-11-03 16:36:38 +0800

    event loop

--- app/js/complex-js/async/async.js
+++ app/js/complex-js/async/async.js
@@ -40,3 +40,80 @@ function fireworks() {
     }, 1000);
   }, 5000)
 }
+
+
+function macrotaskQueue() {
+
+  function hardWork() {//браузер подвисает, пока не выполнится подсчёт
+    let i = 0;
+
+    let start = Date.now();
+
+    function count() {
+
+      // делаем тяжёлую работу
+      for (let j = 0; j < 1000000000; j++) {
+        i++;
+      }
+
+      console.log("До миллиарда посчитано за " + (Date.now() - start) + ' миллисекунд');
+    }
+
+    count();
+  }
+  hardWork();
+
+  function hardWorkWithSetTimeout() {//браузер не подвиснет, т.к. каждый миллион итераций счет прерывается и выполняются другие задачи, если они есть, затем снова запускается счёт. Т.к. у setTimeout нулевая звдержка, то он просто вклинивается через миллион итераций, ставя следующую итерацию в очередь.
+    let i = 0;
+
+    let start = Date.now();
+
+    function count() {
+
+      // делаем тяжёлую работу
+      do {
+        i++;
+      } while (i % 1000000 != 0);
+
+      if (i == 1000000000) {
+        console.log("До миллиарда посчитано за " + (Date.now() - start) + ' миллисекунд');
+      }
+      else{
+        setTimeout(count);
+        // console.log(i);
+      }
+    }
+
+    count();
+  }
+  hardWorkWithSetTimeout();
+
+  function atFirstSetTimeout() {//в браузере есть минимальная задержка в 4 миллисекунды при множестве вложенных вызовов setTimeout. Даже если мы указываем задержку 0, на самом деле она будет равна 4 мс (или чуть больше). Поэтому чем раньше мы запланируем выполнение – тем быстрее выполнится код. В даннном случае - секунды на 4 быстрее, чем предыдущая функция.
+    let counter = document.querySelector('.counter');
+    let i = 0;
+
+    let start = Date.now();
+
+    function count() {
+      // перенесём планирование очередного вызова в начало
+      if (i < 1001000000 - 100000) {
+        counter.innerHTML = i;
+        setTimeout(count);// запланировать новый вызов
+      }
+
+      do {
+        i++;
+      } while (i % 1000000 != 0);
+
+      if (i == 1000000000) {
+        console.log("До миллиарда посчитано за " + (Date.now() - start) + ' миллисекунд');
+      }
+    }
+
+    count();
+  }
+  atFirstSetTimeout();
+
+
+}
+macrotaskQueue()
